56b3b1f2308fe90454ebf1a81738f131675cfb66
KathM00 <kathymontamejia@gmail.com>
2026-08-24 23:18:31 -0400

    feat: incorporar seleccion de genero en el saludo

--- src/presenter.js
+++ src/presenter.js
@@ -1,1 +1,17 @@
 import saludar from "./saludador.js";
+
+const nombreInput = document.querySelector("#nombre-input");
+const generoSelect = document.querySelector("#genero-select");
+const form = document.querySelector("#saludar-form");
+const div = document.querySelector("#resultado-div");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const nombre = nombreInput.value;
+  const genero = generoSelect.value;
+  let fechaActual = new Date();
+  let horaActual = fechaActual.getHours();
+  
+  const mensaje = saludar(nombre, horaActual, genero);
+  div.innerHTML = "<p>" + mensaje + "</p>";
+});
--- src/saludador.js
+++ src/saludador.js
@@ -1,7 +1,10 @@
-export default function saludar(nombre, hora) {
+export default function saludar(nombre, hora, genero) {
   const esManana = hora >= 5 && hora < 12;
   const esTarde = hora >= 12 && hora < 19;
-  const base = esManana ? "Buenos días" : (esTarde ? "Buenas tardes" : "Buenas noches");
+  const base = (esManana && "Buenos días") || (esTarde && "Buenas tardes") || "Buenas noches";
   
-  return base + ", " + nombre;
+  const titulos = { M: "Sr. ", F: "Sra. " };
+  const titulo = titulos[genero] || "";
+  
+  return base + ", " + titulo + nombre;
 }
