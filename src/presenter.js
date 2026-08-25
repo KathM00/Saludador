358e14cf597b813d922a65277e93ac9dfaa059a6
KathM00 <kathymontamejia@gmail.com>
2026-08-24 23:20:24 -0400

    realizar saludo utilizando validacion de edad

--- src/presenter.js
+++ src/presenter.js
@@ -1,6 +1,7 @@
 import saludar from "./saludador.js";
 
 const nombreInput = document.querySelector("#nombre-input");
+const edadInput = document.querySelector("#edad-input");
 const generoSelect = document.querySelector("#genero-select");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
@@ -8,10 +9,11 @@ const div = document.querySelector("#resultado-div");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const nombre = nombreInput.value;
+  const edad = Number.parseInt(edadInput.value);
   const genero = generoSelect.value;
   let fechaActual = new Date();
   let horaActual = fechaActual.getHours();
   
-  const mensaje = saludar(nombre, horaActual, genero);
+  const mensaje = saludar(nombre, horaActual, genero, edad);
   div.innerHTML = "<p>" + mensaje + "</p>";
 });
--- src/saludador.js
+++ src/saludador.js
@@ -1,10 +1,11 @@
-export default function saludar(nombre, hora, genero) {
+export default function saludar(nombre, hora, genero, edad) {
   const esManana = hora >= 5 && hora < 12;
   const esTarde = hora >= 12 && hora < 19;
   const base = (esManana && "Buenos días") || (esTarde && "Buenas tardes") || "Buenas noches";
   
   const titulos = { M: "Sr. ", F: "Sra. " };
-  const titulo = titulos[genero] || "";
+  const esMayor = edad > 30;
+  const titulo = (esMayor && titulos[genero]) || "";
   
   return base + ", " + titulo + nombre;
 }
